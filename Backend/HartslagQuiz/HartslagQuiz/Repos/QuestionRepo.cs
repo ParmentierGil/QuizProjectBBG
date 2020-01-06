f0c751d24a9004dfebfd28c2446c2a9fa8ddf9b5
ParmentierGil <gil.parmentier@student.howest.be>
2020-01-06 15:00:40 +0100

    vs structure

---
+++ Backend/HartslagQuiz/HartslagQuiz/Repos/QuestionRepo.cs
@@ -0,0 +1,25 @@
+﻿using HartslagQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HartslagQuiz.Repos
+{
+    class QuestionRepo
+    {
+        public async Task PostQuestion(Question question)
+        {
+
+            using (SqlConnection con = new SqlConnection())
+            {
+                await con.OpenAsync();
+                using (SqlCommand command = new SqlCommand())
+                {
+
+                }
+            }
+        }
+    }
+}
