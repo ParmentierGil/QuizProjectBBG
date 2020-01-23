f61cbf298d173b4e30c2274edf5170a6a4c1d33c
Gil <gilparmentier@hotmail.com>
2020-01-23 14:50:41 +0100

    socket io working

--- Frontend/Visual_Studio_Code/script/speler_wachtruimte.js
+++ Frontend/Visual_Studio_Code/script/speler_wachtruimte.js
@@ -1,35 +1,35 @@
 var socket;
 
 //#region FUNCTIONS
-//#endregion
 
 //#region GET
-//#endregion
 
 //#region show
-//#endregion
 
 //#region ListenTo
-var nextpage = function() {
+var listenToNextPage = function() {
   var button = document.querySelector(".button");
   button.addEventListener("click", function() {
     //location.href = "quizmaster_vragenscherm.html";
-    socket.emit("startgame", { "join code": "KBIS" });
+    socket.emit("start game", { "join code": "KBIS" });
   });
 };
 
-let listenToSocket = function() {
-  socket.on("game started", function(json) {
-    console.log(json);
-  });
-};
 //#endregion
 
+//#region ListenToSocket
+var listenToSocket = function() {
+  socket.on("game started", function(QA) {
+    console.log(QA);
+  });
+};
+
 //#region init
+
 const init = function() {
-  nextpage();
   socket = io("http://172.30.248.71:5500");
   listenToSocket();
+
   socket.on("connect", function() {
     socket.emit("clientconnected", { data: "I'm connected!" });
   });
