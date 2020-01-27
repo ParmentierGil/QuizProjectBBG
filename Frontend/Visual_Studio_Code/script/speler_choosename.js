1b3051d2814e76fa1616d96838c08bb7c14cc960
Gil <gilparmentier@hotmail.com>
2020-01-27 16:28:02 +0100

    merge html js

--- Frontend/Visual_Studio_Code/script/speler_choosename.js
+++ Frontend/Visual_Studio_Code/script/speler_choosename.js
@@ -1,3 +1,7 @@
+var socket;
+
+var playerId;
+
 //#region FUNCTIONS
 
 //#region GET
@@ -6,39 +10,45 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 //#region ListenTo
-var alertfunctie = function(){
-    var input = document.querySelector(".inputColor2");
-    var submit = document.querySelector(".buttonCodeScreen");
-    var alert = document.querySelector(".alert");
+var listenToKeypress = function() {
+  return (
+    (event.charCode > 64 && event.charCode < 91) ||
+    (event.charCode > 96 && event.charCode < 123)
+  );
+};
+var alertfunctie = function() {
+  var input = document.querySelector(".inputColor2");
+  var submit = document.querySelector(".buttonCodeScreen");
+  var alert = document.querySelector(".alert");
 
-    submit.addEventListener("click", async function(){
-        valid = true;
-
-        if(input.value == "") {
-            alert.style.display = "block";
-            valid = false;
-            await delay(3000);
-            alert.style.display = "none"
-        }
-
-        else{
-            location.href="speler_chooseBTDevice.html";
-        }
-
-        return valid;
-    });
-    
-}
-
-
+  submit.addEventListener("click", async function() {
+    if (input.value == "") {
+      alert.innerHTML = "Vul je naam in";
+      await delay(3000);
+      alert.innerHTML = "";
+    } else {
+      socket.emit("makeplayer", { username: input.value });
+    }
+  });
+};
 //#region init
 const init = function() {
-    alertfunctie();
-  };
+  socket = io("http://172.30.248.137:5500");
 
+  socket.on("connect", function() {
+    socket.emit("clientconnected", { data: "I'm connected!" });
+  });
+  socket.on("playerid", function(data) {
+    console.log(data);
+    localStorage.setItem("playerId", data);
+    location.href = "speler_spelcode.html";
+  });
 
-document.addEventListener('DOMContentLoaded', function(){
-    console.info("Page loaded");
-    init();
+  alertfunctie();
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.info("Page loaded");
+  init();
 });
 //#endregion
